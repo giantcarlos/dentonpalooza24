5f678a3e5953e00086499b0a9aab640cbf14fd7b
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-06-24 21:43:54 -0500

    add lineup, schedule, tickets, merch, and press components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   return (
     <>
       <div>
+        <header>
+        </header>
         <NavBar />
       </div>
     </>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,7 +4,15 @@ function NavBar() {
 
     return (
         <nav>
-            
+           <h1>DENTONPALOOZA 4</h1>
+           <h2>OCTOBER 12, 2024</h2>
+           <ul>
+                <li className="link">Home</li>
+                <li className="link">Lineup</li>
+                <li className="link">Tickets</li>
+                <li className="link">Merch</li>
+                <li className="link">Press</li>
+            </ul>
         </nav>
     )
 }
