a2992e692a10a3ab825787b32f8b7194acc62a7b
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-06-28 14:28:03 -0500

    add lineup poster

--- src/Components/Schedule.jsx
+++ src/Components/Schedule.jsx
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 export default class Schedule extends Component {
   render() {
     return (
-      <div>
+      <div className="schedule">
         <img className="books" src='https://i.imgur.com/N4vr26E.jpeg' alt='Books for Tall People sign at Recycled Books'></img>
+        <img className="poster" src='https://i.imgur.com/s993AaR.jpeg' alt='Lineup poster for Dentonpalooza 4'></img>
       </div>
     )
   }
