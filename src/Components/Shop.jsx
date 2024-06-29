90b8bf3428186c0cf8d0da57ab94b2fc851afa8e
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-06-29 17:42:22 -0500

    add links to prekindle, DMAC, Art Room, and Leukemia Texas

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -7,7 +7,7 @@ export default class Shop extends Component {
         <div className="tickets">
             <h3>TICKET INFORMATION</h3>
             <p>Each ticket is priced at $20</p>
-            <h3>BUY TICKETS</h3>
+            <h3><a href="https://www.prekindle.com/event/49535-dentonpalooza-4-denton" target="_blank">BUY TICKETS ➤</a></h3>
         </div>
         <div className="merch">
             <h3>MERCHANDISE</h3>
@@ -17,7 +17,7 @@ export default class Shop extends Component {
         <div className="beneficiaries">
             <h3>BENEFICIARIES</h3>
             <p>All funds raised are distributed evenly between our beneficiaries.</p>
-            <h3>DMAC / THE ART ROOM / LEUKEMIA TEXAS</h3>
+            <h3><a href="https://dmacdenton.org/" target="_blank">DMAC ➤</a> / <a href="https://theartroomdenton.org/" target="_blank">THE ART ROOM ➤</a> / <a href="https://leukemiatexas.org/" target="_blank">LEUKEMIA TEXAS ➤</a></h3>
         </div>
       </div>
     )
