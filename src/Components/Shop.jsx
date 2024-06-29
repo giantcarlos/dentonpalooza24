3665ed91d0ead95b8c8745f8bbecb31f0a06b7fe
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-06-29 16:53:28 -0500

    add beneficiaries

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -7,13 +7,17 @@ export default class Shop extends Component {
         <div className="tickets">
             <h3>TICKET INFORMATION</h3>
             <p>Each ticket is priced at $20</p>
-            <p>Funds raised are distributed evenly between DMAC, The Art Room, and Leukemia Texas.</p>
             <h3>BUY TICKETS</h3>
         </div>
         <div className="merch">
             <h3>MERCHANDISE</h3>
             <p>Stay tuned for upcoming merchandise.</p>
             <h3>COMING SOON</h3>
+        </div>
+        <div className="beneficiaries">
+            <h3>BENEFICIARIES</h3>
+            <p>Funds raised are distributed evenly between DMAC, The Art Room, and Leukemia Texas.</p>
+            <h3>DMAC / THE ART ROOM / LEUKEMIA TEXAS</h3>
         </div>
       </div>
     )
