a712356b6a85951173c7eec4c781d6ff435b0043
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-06-30 17:23:48 -0500

    change font sizes for shop component

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -5,17 +5,17 @@ export default class Shop extends Component {
     return (
       <div className="shop-grid">
         <div className="tickets">
-            <h3>TICKET INFORMATION</h3>
+            <h3>TICKETS</h3>
             <p>Each ticket is priced at $20</p>
-            <h3><a href="https://www.prekindle.com/event/49535-dentonpalooza-4-denton" target="_blank">BUY TICKETS ➤</a></h3>
+            <h4><a href="https://www.prekindle.com/event/49535-dentonpalooza-4-denton" target="_blank">BUY TICKETS ➤</a></h4>
         </div>
         <div className="merch">
             <h3>MERCHANDISE</h3>
             <p>Stay tuned for upcoming merchandise.</p>
-            <h3>COMING SOON</h3>
+            <h4>COMING SOON</h4>
         </div>
         <div className="beneficiaries">
-            <h3>BENEFICIARIES</h3>
+            <h2>BENEFICIARIES</h2>
             <p>All funds raised are distributed evenly between our beneficiaries.</p>
             <h4><a href="https://dmacdenton.org/" target="_blank">DMAC ➤</a> / <a href="https://theartroomdenton.org/" target="_blank">THE ART ROOM ➤</a> / <a href="https://leukemiatexas.org/" target="_blank">LEUKEMIA TEXAS ➤</a></h4>
         </div>
