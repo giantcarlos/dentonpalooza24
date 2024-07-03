643bff6aefad920e1fe7dde8974926e70e8464b6
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-03 14:46:28 -0500

    fix lineup nav link

--- src/Components/Lineup.jsx
+++ src/Components/Lineup.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Lineup extends Component {
   render() {
     return (
-      <section id="about" className="lineup">
+      <section id="lineup" className="lineup">
         <img className="books" src='https://i.imgur.com/N4vr26E.jpeg' alt='Books for Tall People sign at Recycled Books'></img>
         <img className="poster" src='https://i.imgur.com/s993AaR.jpeg' alt='Lineup poster for Dentonpalooza 4'></img>
         <h1 className="ticket-text">TICKETS ARE NOW ON SALE</h1>
