49433f69711e8ee67c4caf99df0281e9fbc7edf6
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-03 15:08:20 -0500

    add press section header

--- src/Components/Press.jsx
+++ src/Components/Press.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Press extends Component {
   render() {
     return (
-      <section id="press">
-        Press
+      <section id="press" className="press">
+        <h2>PRESS FOR DENTONPALOOZA 4</h2>
       </section>
     )
   }
