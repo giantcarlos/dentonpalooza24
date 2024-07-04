024ffc78c4898844b06b8d0d74bd0ad8591954bd
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-04 00:35:13 -0500

    float footer details on both sides

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,10 @@ export default class Footer extends Component {
   render() {
     return (
       <section className="footer">
-        <div className="footer-detail">Follow us on Instragram&nbsp;
+        <div className="footer-social">Follow us on Instragram&nbsp;
           <a href='https://www.instagram.com/dentonpalooza/' target="_blank">@dentonpalooza</a>
         </div>
-        <div className="footer-detail">Website designed and maintained by&nbsp;
+        <div className="footer-dev">Website designed and maintained by&nbsp;
           <a href='https://www.giantcarlos.com/' target="_blank">Gian Carlos</a>
         </div>
       </section>
