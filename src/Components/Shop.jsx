c99a598fd665a0f3e7e71e320f401ab6710e7480
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-06 01:18:13 -0500

    edit shop font

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -6,17 +6,17 @@ export default class Shop extends Component {
       <section className="shop-grid">
         <div id="tickets" className="tickets">
             <h3>TICKETS</h3>
-            <h4>Each ticket is priced at $20</h4>
+            <h4 className="small-text">Each ticket is priced at $20</h4>
             <h4><a href="https://www.prekindle.com/event/49535-dentonpalooza-4-denton" target="_blank">BUY TICKETS ➤</a></h4>
         </div>
         <div id="merch" className="merch">
             <h3>MERCHANDISE</h3>
-            <h4>Stay tuned for upcoming merchandise.</h4>
+            <h4 className="small-text">Stay tuned for upcoming merchandise</h4>
             <h4>COMING SOON</h4>
         </div>
         <div className="beneficiaries">
             <h3>BENEFICIARIES</h3>
-            <h4>All funds raised benefit these charities.</h4>
+            <h4 className="small-text">All funds raised benefit these charities</h4>
             <h4><a href="https://dmacdenton.org/" target="_blank">DMAC ➤</a> / <a href="https://theartroomdenton.org/" target="_blank">THE ART ROOM ➤</a> / <a href="https://leukemiatexas.org/" target="_blank">LEUKEMIA TEXAS ➤</a></h4>
         </div>
       </section>
