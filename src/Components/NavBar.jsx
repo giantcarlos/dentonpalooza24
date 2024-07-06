28fcb90551871ddb74f6d6ea7e023c12f124081e
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-06 00:14:51 -0500

    add nav offset to lineup

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -22,7 +22,7 @@ function NavBar() {
 
     const scrollWithOffset2 = (el) => {
         const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-        const yOffset = 200; 
+        const yOffset = 160; 
         window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
     }
     
