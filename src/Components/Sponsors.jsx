b62b4065c63b97110b9a375e5d52e1ba81f348bd
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-08 16:50:37 -0500

    add MBH company logo

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -7,6 +7,7 @@ export default class Sponsors extends Component {
             <a href='https://www.barnhartsbarbershop.com/' className='image-gray' target="_blank"><img src={"https://i.imgur.com/nJXwjip.png"} alt='logo of Barnharts Barbershop' /></a>
             <a href='https://www.morefuncomicsandgames.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/k8Vheyt.png"} alt='logo of More Fun Comics and Games' /></a>
             <a href='http://recycledbooks.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/6F5XWoE.png"} alt='logo of Recycled Books' /></a>
+            <a href='https://www.familylaw-tx.com/' className='image-reverse-gray-zoom' target="_blank"><img src={"https://i.imgur.com/arOxCaa.png"} alt='logo of MBH Family Law' /></a>
             <a href='https://rubberglovesdenton.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/6ZhmD7H.png"} alt='logo of Rubber Gloves' /></a>
             <div className="message">A big thanks to our sponsors!</div>
         </div>
