c28f8bcf1c7182eacda3bb73492aefa732669b65
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-14 13:53:02 -0500

    remove body animation. add sponsor stagger animation

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
+import { motion } from 'framer-motion';
 
 export default class Sponsors extends Component {
   render() {
+    const container = {
+      hidden: { opacity: 0 },
+      show: {
+        opacity: 1,
+        transition: {
+          staggerChildren: 0.25
+        }
+      }
+    }
+
+    const item = {
+      hidden: { opacity: 0 },
+      show: { opacity: 1 }
+    }
+
     return (
-        <div className="image-grid">
-            <a href='https://www.barnhartsbarbershop.com/' className='image-gray' target="_blank"><img src={"https://i.imgur.com/nJXwjip.png"} alt='logo of Barnharts Barbershop' /></a>
-            <a href='https://www.morefuncomicsandgames.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/k8Vheyt.png"} alt='logo of More Fun Comics and Games' /></a>
-            <a href='http://recycledbooks.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/6F5XWoE.png"} alt='logo of Recycled Books' /></a>
-            <a href='https://rubberglovesdenton.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/6ZhmD7H.png"} alt='logo of Rubber Gloves' /></a>
-            <a href='https://www.familylaw-tx.com/' className='image-reverse-gray-zoom' target="_blank"><img src={"https://i.imgur.com/arOxCaa.png"} alt='logo of MBH Family Law' /></a>
-            <div className="message">A big thanks to our sponsors!</div>
-        </div>
+        <motion.div variants={container} initial="hidden" animate="show" className="image-grid">
+            <motion.a variants={item} href='https://www.barnhartsbarbershop.com/' className='image-gray' target="_blank"><img src={"https://i.imgur.com/nJXwjip.png"} alt='logo of Barnharts Barbershop' /></motion.a>
+            <motion.a variants={item} href='https://www.morefuncomicsandgames.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/k8Vheyt.png"} alt='logo of More Fun Comics and Games' /></motion.a>
+            <motion.a variants={item} href='http://recycledbooks.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/6F5XWoE.png"} alt='logo of Recycled Books' /></motion.a>
+            <motion.a variants={item} href='https://rubberglovesdenton.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/6ZhmD7H.png"} alt='logo of Rubber Gloves' /></motion.a>
+            <motion.a variants={item} href='https://www.familylaw-tx.com/' className='image-reverse-gray-zoom' target="_blank"><img src={"https://i.imgur.com/arOxCaa.png"} alt='logo of MBH Family Law' /></motion.a>
+            <motion.div variants={item} className="message">A big thanks to our sponsors!</motion.div>
+        </motion.div>
     )
   }
 }
