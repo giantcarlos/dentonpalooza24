850d45aa7d55c628c2080cc82e082bae527f9227
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-15 13:08:13 -0500

    add scroll animation for lineup poster

--- src/Components/Lineup.jsx
+++ src/Components/Lineup.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { motion } from 'framer-motion';
 
 export default class Lineup extends Component {
   render() {
     return (
       <section id="lineup" className="lineup">
         <img className="books" src='https://i.imgur.com/N4vr26E.jpeg' alt='Books for Tall People sign at Recycled Books'></img>
-        <img className="poster" src='https://i.imgur.com/s993AaR.jpeg' alt='Lineup poster for Dentonpalooza 4'></img>
+        <motion.img initial={{ y: 400 }} whileInView={{ y: 0 }} transition={{ duration: 1.25 }} viewport={{ margin: "350px", once: true }} className="poster" src='https://i.imgur.com/s993AaR.jpeg' alt='Lineup poster for Dentonpalooza 4'></motion.img>
         <h1 className="ticket-text">Set times coming soon!</h1>
         <h2 className="ticket-text2">Check back closer to the event.</h2>
       </section>
