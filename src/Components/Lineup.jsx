755f15857daff91aded205f2d495b0fd7368e6e1
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-15 13:27:15 -0500

    edit lineup poster scroll transition speed

--- src/Components/Lineup.jsx
+++ src/Components/Lineup.jsx
@@ -6,7 +6,7 @@ export default class Lineup extends Component {
     return (
       <section id="lineup" className="lineup">
         <img className="books" src='https://i.imgur.com/N4vr26E.jpeg' alt='Books for Tall People sign at Recycled Books'></img>
-        <motion.img initial={{ y: 200 }} whileInView={{ y: 0 }} transition={{ duration: .75 }} viewport={{ once: true }} className="poster" src='https://i.imgur.com/s993AaR.jpeg' alt='Lineup poster for Dentonpalooza 4'></motion.img>
+        <motion.img initial={{ y: 150 }} whileInView={{ y: 0 }} transition={{ duration: .625 }} viewport={{ once: true }} className="poster" src='https://i.imgur.com/s993AaR.jpeg' alt='Lineup poster for Dentonpalooza 4'></motion.img>
         <h1 className="ticket-text">Set times coming soon!</h1>
         <h2 className="ticket-text2">Check back closer to the event.</h2>
       </section>
