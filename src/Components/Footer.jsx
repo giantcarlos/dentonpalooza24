201d26f3e39d7f293f2ea85322b81c199028ab00
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-15 14:02:25 -0500

    add fotter scroll animation

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { motion } from 'framer-motion';
 
 export default class Footer extends Component {
   render() {
     return (
-      <section className="footer">
+      <motion.section initial={{ opacity: 0, y: 250 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .375 }} viewport={{ margin: "250px", once: true }}className="footer">
         <div className="footer-social">Follow us on Instragram&nbsp;
           <a href='https://www.instagram.com/dentonpalooza/' target="_blank">@dentonpalooza</a>
         </div>
         <div className="footer-dev">Website designed and maintained by&nbsp;
           <a href='https://www.giantcarlos.com/' target="_blank">Gian Carlos</a>
         </div>
-      </section>
+      </motion.section>
     )
   }
 }
