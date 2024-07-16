96eb9bb24bbb6c9d3e607308f20c34a28376cda4
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-15 22:54:02 -0500

    disable lineup poster animation for mobile

--- src/Components/Lineup.jsx
+++ src/Components/Lineup.jsx
@@ -3,10 +3,19 @@ import { motion } from 'framer-motion';
 
 export default class Lineup extends Component {
   render() {
+
+    let variants = {};
+  const isMobile = window.innerWidth < 900;
+  if (!isMobile) {
+    variants = {
+      start: { y: 150 },
+      end: { y: 0 }
+    }}
+
     return (
       <section id="lineup" className="lineup">
         <img className="books" src='https://i.imgur.com/N4vr26E.jpeg' alt='Books for Tall People sign at Recycled Books'></img>
-        <motion.img initial={{ y: 150 }} whileInView={{ y: 0 }} transition={{ duration: .375 }} viewport={{ once: true }} className="poster" src='https://i.imgur.com/s993AaR.jpeg' alt='Lineup poster for Dentonpalooza 4'></motion.img>
+        <motion.img variants={variants} initial="start" whileInView="end" transition={{duration: .375}} viewport={{ once: true }} className="poster" src='https://i.imgur.com/s993AaR.jpeg' alt='Lineup poster for Dentonpalooza 4'></motion.img>
         <h1 className="ticket-text">Set times coming soon!</h1>
         <h2 className="ticket-text2">Check back closer to the event.</h2>
       </section>
