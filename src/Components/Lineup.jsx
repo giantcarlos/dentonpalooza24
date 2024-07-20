053b9b76bd430510d6daaed0ceeff1e6d3c00164
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-19 23:59:55 -0500

    optimize images

--- src/Components/Lineup.jsx
+++ src/Components/Lineup.jsx
@@ -14,8 +14,8 @@ export default class Lineup extends Component {
 
     return (
       <section id="lineup" className="lineup">
-        <img className="books" src='https://i.imgur.com/N4vr26E.jpeg' alt='Books for Tall People sign at Recycled Books'></img>
-        <motion.img variants={variants} initial="start" whileInView="end" transition={{duration: .425}} viewport={{ once: true }} className="poster" src='https://i.imgur.com/s993AaR.jpeg' alt='Lineup poster for Dentonpalooza 4'></motion.img>
+        <img className="books" src='https://i.imgur.com/e1wxIZC.jpeg' alt='Books for Tall People sign at Recycled Books'></img>
+        <motion.img variants={variants} initial="start" whileInView="end" transition={{duration: .425}} viewport={{ once: true }} className="poster" src='https://i.imgur.com/vYBFSvV.jpeg' alt='Lineup poster for Dentonpalooza 4'></motion.img>
         <h1 className="ticket-text">Set times coming soon!</h1>
         <h2 className="ticket-text2">Check back closer to the event.</h2>
       </section>
