5529003c225b9638fbbcfcd48b1d26acbd2763d2
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-07-29 13:05:20 -0500

    change lineup animation to fade in

--- src/Components/Lineup.jsx
+++ src/Components/Lineup.jsx
@@ -4,18 +4,15 @@ import { motion } from 'framer-motion';
 export default class Lineup extends Component {
   render() {
 
-    let variants = {};
-  const isMobile = window.innerWidth < 900;
-  if (!isMobile) {
-    variants = {
-      start: { y: 175 },
-      end: { y: 0 }
-    }}
+    let variants =  {
+      start: { opacity: 0 },
+      end: { opacity: 1 }
+    }
 
     return (
       <section id="lineup" className="lineup">
         <img className="books" src='https://i.imgur.com/e1wxIZC.jpeg' alt='Books for Tall People sign at Recycled Books'></img>
-        <motion.img variants={variants} initial="start" whileInView="end" transition={{duration: .425}} viewport={{ once: true }} className="poster" src='https://i.imgur.com/vYBFSvV.jpeg' alt='Lineup poster for Dentonpalooza 4'></motion.img>
+        <motion.img variants={variants} initial="start" whileInView="end" transition={{duration: .75}} viewport={{ once: true }} className="poster" src='https://i.imgur.com/vYBFSvV.jpeg' alt='Lineup poster for Dentonpalooza 4'></motion.img>
         <h1 className="ticket-text">Set times coming soon!</h1>
         <h2 className="ticket-text2">Check back closer to the event.</h2>
       </section>
