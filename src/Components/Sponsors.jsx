19b1acba346f9ccf7184f6fcfa4415d5640b0ee3
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-08-29 17:00:04 -0500

    add more sponsors, replace EIT with Mad max truck image

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -9,7 +9,7 @@ export default class Sponsors extends Component {
         opacity: 1,
         transition: {
           delayChildren:.375,
-          staggerChildren: 0.15
+          staggerChildren: 0.1125
         }
       }
     }
@@ -21,9 +21,13 @@ export default class Sponsors extends Component {
 
     return (
         <motion.div variants={container} initial="hidden" animate="show" className="image-grid">
+            <motion.a variants={item} href='https://www.missangelinesdenton.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/GBB5S1l.png"} alt='logo of Miss Angelines' /></motion.a>
             <motion.a variants={item} href='https://www.barnhartsbarbershop.com/' className='image-gray' target="_blank"><img src={"https://i.imgur.com/nJXwjip.png"} alt='logo of Barnharts Barbershop' /></motion.a>
+            <motion.a variants={item} href='https://danssilverleaf.com/' target="_blank"><img src={"https://i.imgur.com/jHoY46F.png"} alt='logo of Dans Silverleaf' /></motion.a>
+            <motion.a variants={item} href='https://www.eastsidedtx.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/CWrmgQg.jpg"} alt='logo of Eastside' /></motion.a>
             <motion.a variants={item} href='https://www.familylaw-tx.com/' className='image-reverse-gray-out' target="_blank"><img src={"https://i.imgur.com/dqU12Pm.jpeg"} alt='logo of MBH Family Law' /></motion.a>
             <motion.a variants={item} href='https://www.morefuncomicsandgames.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/gTzwN6j.png"} alt='logo of More Fun Comics and Games' /></motion.a>
+            <motion.a variants={item} href='https://oakstreetdrafthouse.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/L1Jf7L6.png"} alt='logo of Oak Street Drafthouse' /></motion.a>
             <motion.a variants={item} href='https://rahrbrewing.com/' className='image-reverse-gray-zoom2' target="_blank"><img src={"https://i.imgur.com/ZJaZOZ1.png"} alt='logo of Rahr & Sons' /></motion.a>
             <motion.a variants={item} href='http://recycledbooks.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/taLVz7q.png"} alt='logo of Recycled Books' /></motion.a>
             <motion.a variants={item} href='https://rubberglovesdenton.com/' className='image-reverse-gray-zoom3' target="_blank"><img src={"https://i.imgur.com/6ZhmD7H.png"} alt='logo of Rubber Gloves' /></motion.a>
