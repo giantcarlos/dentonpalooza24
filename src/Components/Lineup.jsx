e1c2aec2672d28f5d19f5ffffbd0b11ad8f50991
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-09-04 13:36:38 -0500

    update new lineup poster

--- src/Components/Lineup.jsx
+++ src/Components/Lineup.jsx
@@ -15,7 +15,7 @@ export default class Lineup extends Component {
     return (
       <section id="lineup" className="lineup">
         <img className="books" src='https://i.imgur.com/e1wxIZC.jpeg' alt='Books for Tall People sign at Recycled Books'></img>
-        <motion.img variants={variants} initial="start" whileInView="end" transition={{duration: .75}} viewport={{ once: true }} className="poster" src='https://i.imgur.com/vYBFSvV.jpeg' alt='Lineup poster for Dentonpalooza 4'></motion.img>
+        <motion.img variants={variants} initial="start" whileInView="end" transition={{duration: .75}} viewport={{ once: true }} className="poster" src='https://i.imgur.com/XG1Qgto.png' alt='Lineup poster for Dentonpalooza 4'></motion.img>
         <h1 className="ticket-text">Set times coming soon!</h1>
         <h2 className="ticket-text2">Check back closer to the event.</h2>
       </section>
