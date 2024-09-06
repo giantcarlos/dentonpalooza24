2c5eb3acf9754a2847aa631081983d4f2fe351e1
Gian Taino Carlos <giantainocarlos@gmail.com>
2024-09-05 21:16:42 -0500

    add image of the Von Erich brothers

--- src/Components/Images.jsx
+++ src/Components/Images.jsx
@@ -10,6 +10,7 @@ export default class Images extends Component {
     }
     return (
       <motion.div variants={item} initial="hidden" animate="show" transition={{duration: 1}} className='image-container'>
+        <div className='pic' id='pic4' alt="Von Erich brothers in their Iron Claw pose." />
         <div className='pic' id='pic3' alt="Carl Finch playing the accordion."/>
         <div className='pic' id='pic2' alt="Everything is Terrible on stage."/>
         <div className='pic' id='pic1' alt="Singer of Riverboat Gamblers crowdsurfing." />
